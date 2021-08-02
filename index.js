e39223cdf278dfa35f7dfe69bc96edf0279c3943
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 14:10:34 -0300

    Ajustes de Teste

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => { res.send('Hello World!')})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
