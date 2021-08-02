133c6f6acb716ab738b72f9896f2f07d6e3d0ac7
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 14:05:35 -0300

    Ajustes de LOG

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) =>  res.send('Hello World!'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) =>  res.send('Hello World!'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+})
