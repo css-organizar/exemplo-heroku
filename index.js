2e0a05bca2b04de4327e78aeba1301df56ed582b
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 13:48:01 -0300

    .

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 3030
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log('Example app listening at http://localhost:${port}')
-})
+app.listen(port)
