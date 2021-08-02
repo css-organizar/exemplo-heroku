1c48cb5ada5e227ca692f1afd57859b91da1814e
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 13:01:55 -0300

    Ajustes de PORTA

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req,res) => res.send('Teste Heroku'));
 
 const PORT = process.env.PORT || '3000';
 
-app.set("port",PORT);
+// app.set("port",PORT);
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
