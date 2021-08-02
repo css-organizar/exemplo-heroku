e7e4e145cac65d9fb9e19b2a0e0731c4c2b9cfa7
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 13:11:48 -0300

    .

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || '3000';
 
 // app.set("port",PORT);
 
-app.listen(process.env.PORT||8080, function(){
+app.listen(process.env.PORT||'8080', function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
