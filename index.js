92e957da6fc8348c565eca14cba404acb4e5f4e5
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 13:53:31 -0300

    Substituindo o código inteiro pelo exemplo do Heroku

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const app = express()
-const port = 3030
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port)
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
