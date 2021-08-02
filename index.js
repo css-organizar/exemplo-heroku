654e63bf7c77e41604cfb49d7006681727f1a025
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 13:38:55 -0300

    .

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', (req,res) => res.send('Teste Heroku'));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const PORT = process.env.PORT || '3000';
-
-app.set("port",PORT);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
