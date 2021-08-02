512c222f9ee67d78a79ac4ee52900bc9513dc4a7
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 13:30:24 -0300

    Ajuste de Start Script

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', (req,res) => res.send('Teste Heroku'));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const PORT = process.env.PORT || '3000';
-
-// app.set("port",PORT);
-
-app.listen(process.env.PORT||'8080', function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
