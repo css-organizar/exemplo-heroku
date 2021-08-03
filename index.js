dfdf21be206eb3a47ffb85c693fd8fe93276ba31
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 22:57:13 -0300

    Ajuste no Status Code do JWT

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function verifyJWT(req, res, next){
     if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
     
     jwt.verify(token, process.env.SECRET, function(err, decoded) {
-      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
       
       // se tudo estiver ok, salva no request para uso posterior
       req.userId = decoded.id;
