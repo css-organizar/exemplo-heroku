9eb385dffd57f886409cb5c92437fc09ade360ba
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-03 18:03:31 -0300

    Update de Código da Subir Versão no Deploy - Erro na Heroku CLI

--- knexfile.js
+++ knexfile.js
@@ -2,20 +2,6 @@ require('dotenv').config();
 
 module.exports = {
 
-    // development: {
-    //   client: 'sqlite3',
-    //   connection: {
-    //     filename: './src/database/db.sqlite'
-    //   },
-    //   migrations:{
-    //     directory: './src/database/migrations'
-    //   },
-    //   seeds: {
-    //     directory: './src/database/seeds'
-    //   },
-    //   useNullAsDefault: true,
-    // },
-
     development: {
         client: 'postgresql',
         connection: process.env.DATABASE_URL,
