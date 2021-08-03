0984f94454bc3ebf6305704eae8f92eeb5322efb
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 21:00:52 -0300

    Ajuste na rota db

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ applicationServer.get('/db', async (req, res) => {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      res.send(results);
       client.release();
     } catch (err) {
       console.error(err);
