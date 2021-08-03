2bf01265a073bda82e6373b0c61031fbaf7e24b8
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-03 14:34:50 -0300

    Ajustes de Conexão com o PostgreSQL Local e Remoto

--- knexfile.js
+++ knexfile.js
@@ -2,40 +2,55 @@ require('dotenv').config();
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/db.sqlite'
-    },
-    migrations:{
-      directory: './src/database/migrations'
-    },
-    seeds: {
-      directory: './src/database/seeds'
-    },
-    useNullAsDefault: true,
-  },
+    // development: {
+    //   client: 'sqlite3',
+    //   connection: {
+    //     filename: './src/database/db.sqlite'
+    //   },
+    //   migrations:{
+    //     directory: './src/database/migrations'
+    //   },
+    //   seeds: {
+    //     directory: './src/database/seeds'
+    //   },
+    //   useNullAsDefault: true,
+    // },
 
-  staging: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './src/database/migrations'
+    development: {
+        client: 'postgresql',
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: './src/database/migrations'
+        },
+        seeds: {
+            directory: './src/database/seeds'
+        },
     },
-    seeds: {
-      directory: './src/database/seeds'
-    },
-  },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './src/database/migrations'
+    staging: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: './src/database/migrations'
+        },
+        seeds: {
+            directory: './src/database/seeds'
+        },
     },
-    seeds: {
-      directory: './src/database/seeds'
-    },
-  }
+
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: './src/database/migrations'
+        },
+        seeds: {
+            directory: './src/database/seeds'
+        },
+    }
 
 };
--- src/database/connection.js
+++ src/database/connection.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.production);
+const connection = knex(configuration.development);
 
 module.exports = connection;
