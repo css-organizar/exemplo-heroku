094eaabc939e7543e2d776c841a3024ff6488229
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-03 03:56:27 -0300

    Ajustes das Strings de Conexão com o Banco de Dados

--- knexfile.js
+++ knexfile.js
@@ -1,76 +1,25 @@
 require('dotenv').config();
 
-// Update with your config settings.
-
-// module.exports = 
-
 module.exports = {
 
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './src/database/db.sqlite'
-  //   },
-  //   migrations:{
-  //     directory: './src/database/migrations'
-  //   },
-  //   useNullAsDefault: true,
-  // },
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite'
+    },
+    migrations:{
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
 
-  // development: {
-  //   client: 'pg',
-  //   connection: {
-  //     url: process.env.DATABASE_URL,
-  //     charset: 'utf8',
-  //   },
-  // },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // staging: {
-  //   client: 'pg',
-  //   connection: {
-  //     url: process.env.DATABASE_URL,
-  //     charset: 'utf8',
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations:{
-  //     directory: './src/database/migrations'
-  //   },
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './src/database/migrations'
+    },
+  },
 
   production: {
     client: 'pg',
