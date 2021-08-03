9fa51f7846aeae6bcb94f4b70d776609b4f1c62b
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-02 21:15:43 -0300

    Adicionado JWT

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
+
 const express = require('express')
 
 /**Utilitários para trabalhar com caminhos de arquivos e diretórios */
@@ -30,6 +33,24 @@ applicationServer.set('view engine', 'ejs')
 
 /**Métodos da API */
 
+applicationServer.post('/login', (req, res, next) => {
+  //esse teste abaixo deve ser feito no seu banco de dados
+  if(req.body.user === 'luiz' && req.body.password === '123'){
+    //auth ok
+    const id = 1; //esse id viria do banco de dados
+    const token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 300 // expires in 5min
+    });
+    return res.json({ auth: true, token: token });
+  }
+  
+  res.status(500).json({message: 'Login inválido!'});
+})
+
+applicationServer.post('/logout', function(req, res) {
+    res.json({ auth: false, token: null });
+})
+
 applicationServer.get('/', (req, res) => { res.send('Hello World!')})
 
 applicationServer.get('/db', async (req, res) => {
@@ -45,6 +66,29 @@ applicationServer.get('/db', async (req, res) => {
     }
   })
 
+applicationServer.get('/clientes', (req, res, next) => { 
+    console.log("Retornou todos clientes!");
+    res.json([{id:1,nome:'luiz'}]);
+  }) 
+
+applicationServer.get('/clientesjwt', verifyJWT, (req, res, next) => { 
+      console.log("Retornou todos clientes!");
+      res.json([{id:1,nome:'luiz'}]);
+  })
+
 /**Start da API */
 
 applicationServer.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function verifyJWT(req, res, next){
+    const token = req.headers['x-access-token'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      
+      // se tudo estiver ok, salva no request para uso posterior
+      req.userId = decoded.id;
+      next();
+    });
+}
