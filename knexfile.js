aa61321671ae5cce496bdf109b219b5a11d432df
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-03 03:05:59 -0300

    .

--- knexfile.js
+++ knexfile.js
@@ -2,16 +2,16 @@
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/db.sqlite'
-    },
-    migrations:{
-      directory: './src/database/migrations'
-    },
-    useNullAsDefault: true,
-  },
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './src/database/db.sqlite'
+  //   },
+  //   migrations:{
+  //     directory: './src/database/migrations'
+  //   },
+  //   useNullAsDefault: true,
+  // },
 
   // development: {
   //   client: 'pg',
@@ -37,20 +37,20 @@ module.exports = {
   //   }
   // },
 
-  staging: {
-    client: 'pg',
-    connection: {
-      url: process.env.DATABASE_URL,
-      charset: 'utf8',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations:{
-      directory: './src/database/migrations'
-    },
-  },
+  // staging: {
+  //   client: 'pg',
+  //   connection: {
+  //     url: process.env.DATABASE_URL,
+  //     charset: 'utf8',
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations:{
+  //     directory: './src/database/migrations'
+  //   },
+  // },
 
   // production: {
   //   client: 'postgresql',
