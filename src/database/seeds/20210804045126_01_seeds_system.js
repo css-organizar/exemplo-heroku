2879fd971cc940bf102b79e4b522b50399bd9303
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-04 21:47:25 -0300

    Ajuste da Data inicial do registro do sistema

---
+++ src/database/seeds/20210804045126_01_seeds_system.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+var crypto = require("crypto");
+
+exports.seed = function(knex) {
+    return knex('system')
+        .then(function() {
+            var application_token = crypto.randomBytes(32).toString('hex');
+            var expirationDate = new Date();
+            expirationDate.setDate(expirationDate.getDate() + 90);
+
+            console.log(expirationDate);
+
+            return knex('system').insert([{
+                    application_token: application_token,
+                    expiration: expirationDate
+                }])
+                .onConflict('application_token')
+                .ignore();
+        });
+};
