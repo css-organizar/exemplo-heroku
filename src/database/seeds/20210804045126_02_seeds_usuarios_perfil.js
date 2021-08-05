208a962023097d9711b6cae65abcb7b62b412d24
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-08-04 21:47:09 -0300

    Removido o "id" do valor na seed

--- src/database/seeds/20210804045126_02_seeds_usuarios_perfil.js
+++ src/database/seeds/20210804045126_02_seeds_usuarios_perfil.js
@@ -1,14 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
 exports.seed = function(knex) {
     return knex('usuario_perfil')
         .then(function() {
-            const dataAtual = new Date();
             return knex('usuario_perfil')
                 .insert([{
-                        id: 1,
                         descricao: 'administrador'
                     },
                     {
-                        id: 2,
                         descricao: 'usuario'
                     }
                 ])
